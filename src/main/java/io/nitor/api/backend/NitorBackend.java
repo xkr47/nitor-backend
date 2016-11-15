2530c8f24d8aa7bee926aa9d4b43e3a725b0682a
Mikko Tiihonen <mikko.tiihonen@nitorcreations.com>
2016-11-15 13:23:30 +0200

    Enable compression

--- src/main/java/io/nitor/api/backend/NitorBackend.java
+++ src/main/java/io/nitor/api/backend/NitorBackend.java
@@ -2,7 +2,6 @@ package io.nitor.api.backend;
 
 import static com.nitorcreations.core.utils.KillProcess.killProcessUsingPort;
 import static io.vertx.core.http.ClientAuth.REQUEST;
-import static io.vertx.core.http.ClientAuth.REQUIRED;
 import static java.lang.Integer.getInteger;
 import static java.lang.System.getProperty;
 import static java.lang.System.setProperty;
@@ -10,11 +9,8 @@ import static java.lang.System.setProperty;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Launcher;
 import io.vertx.core.http.HttpServerOptions;
-import io.vertx.core.net.JksOptions;
 import io.vertx.core.net.OpenSSLEngineOptions;
 import io.vertx.core.net.PemKeyCertOptions;
-import io.vertx.core.net.PemTrustOptions;
-import io.vertx.core.net.TCPSSLOptions;
 import io.vertx.ext.web.Router;
 
 public class NitorBackend extends AbstractVerticle
@@ -41,17 +37,11 @@ public class NitorBackend extends AbstractVerticle
                 .setUseAlpn(true)
                 .setClientAuth(REQUEST)
                 .setReuseAddress(true)
+                .setCompressionSupported(true)
                 .setSslEngineOptions(new OpenSSLEngineOptions())
-                /*.setPemTrustOptions(new PemTrustOptions()
-                    .addCertPath("certs/localhost.chain"))*/
                 .setPemKeyCertOptions(new PemKeyCertOptions()
                     .setKeyPath("certs/localhost.key.clear")
                     .setCertPath("certs/localhost.crt"))
-                /*.setTrustStoreOptions(
-                    new JksOptions().
-                        setPath("/path/to/your/truststore.jks").
-                        setPassword("password-of-your-truststore")
-                )*/
                 )
             .requestHandler(router::accept)
             .listen(listenPort);
