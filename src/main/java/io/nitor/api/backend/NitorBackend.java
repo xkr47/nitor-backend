d3b71e9fa122f67ecc062d4ee79f401e88278217
Jonas Berlin <jonas.berlin@nitorcreations.com>
2016-11-15 14:34:09 +0200

    Add /proxy reverse proxy to localhost:8080

--- src/main/java/io/nitor/api/backend/NitorBackend.java
+++ src/main/java/io/nitor/api/backend/NitorBackend.java
@@ -1,17 +1,21 @@
 package io.nitor.api.backend;
 
+import io.nitor.api.backend.Proxy.RejectReason;
+import io.nitor.api.backend.Proxy.Target;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Launcher;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.OpenSSLEngineOptions;
+import io.vertx.core.net.PemKeyCertOptions;
+import io.vertx.ext.web.Router;
+
 import static com.nitorcreations.core.utils.KillProcess.killProcessUsingPort;
 import static io.vertx.core.http.ClientAuth.REQUEST;
 import static java.lang.Integer.getInteger;
 import static java.lang.System.getProperty;
 import static java.lang.System.setProperty;
-
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Launcher;
-import io.vertx.core.http.HttpServerOptions;
-import io.vertx.core.net.OpenSSLEngineOptions;
-import io.vertx.core.net.PemKeyCertOptions;
-import io.vertx.ext.web.Router;
 
 public class NitorBackend extends AbstractVerticle
 {
@@ -34,6 +38,22 @@ public class NitorBackend extends AbstractVerticle
         router.get("/healthCheck").handler(routingContext -> {
            routingContext.response().sendFile("pom.xml");
         });
+
+        HttpClient client = vertx.createHttpClient(new HttpClientOptions()
+                .setConnectTimeout(10)
+                .setIdleTimeout(120)
+                .setMaxPoolSize(1000)
+                .setMaxWaitQueueSize(20)
+                .setTryUseCompression(false));
+        Proxy proxy = new Proxy(client,
+                (request, isTls) -> new Target("localhost", 8080, "/", "suchhost"),
+                (request, status, reason, detail) -> {
+            String statusMsg = detail != null ? detail : reason == RejectReason.noHostHeader ? "Exhausted resources while trying to extract Host header from the request" : "";
+            request.response().setStatusCode(status);
+            request.response().headers().set("content-type", "text/plain;charset=UTF-8");
+            request.response().end(statusMsg);
+        });
+        router.get("/proxy").handler(routingContext -> proxy.handle(routingContext.request(), true));
 
         vertx.createHttpServer(
             new HttpServerOptions()
---
+++ src/main/java/io/nitor/api/backend/Proxy.java
@@ -0,0 +1,157 @@
+package io.nitor.api.backend;
+
+import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.streams.Pump;
+
+import java.time.Clock;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.regex.Pattern;
+
+/**
+ * Reverse HTTP proxy, ported to Java from https://github.com/xkr47/vhostproxy4/
+ */
+public class Proxy {
+
+    private final HttpClient client;
+    private final TargetResolver targetResolver;
+    private final ErrorHandler errorHandler;
+
+    public interface TargetResolver {
+        Target resolveNextHop(HttpServerRequest request, boolean isTls);
+    }
+
+    public interface ErrorHandler {
+        void fail(HttpServerRequest sreq, int status, RejectReason reason, String detail);
+    }
+
+    public Proxy(HttpClient client, TargetResolver targetResolver, ErrorHandler errorHandler) {
+        this.client = client;
+        this.targetResolver = targetResolver;
+        this.errorHandler = errorHandler;
+    }
+
+    public static class Target {
+        public String socketHost;
+        public int socketPort;
+        public String uri;
+        public String hostHeader;
+
+        public Target() {}
+
+        public Target(String socketHost, int socketPort, String uri, String hostHeader) {
+            this.socketHost = socketHost;
+            this.socketPort = socketPort;
+            this.uri = uri;
+            this.hostHeader = hostHeader;
+        }
+    }
+
+    public enum RejectReason {
+        incomingRequestFail,
+        outgoingRequestFail,
+        incomingResponseFail,
+        noHostHeader,
+    }
+
+    static final Set<String> hopByHopHeaders = new HashSet<String>(){
+        {
+            add("connection");
+            add("keep-alive");
+            add("proxy-authenticate");
+            add("proxy-authorization");
+            add("te");
+            add("trailer");
+            add("transfer-encoding");
+            add("upgrade");
+            // TODO http2 hbh headers
+        }
+    };
+
+    static final Pattern connectionHeaderValueRE = Pattern.compile("\\s*,[\\s,]*+"); // from RFC2616
+
+    static final int serverIdleTimeout = 60;
+
+    static final String keepAliveHeaderValue = "timeout=" + serverIdleTimeout;
+
+    static final String requestIdHeader = "X-Request-Id";
+
+    static void copyEndToEndHeaders(MultiMap from, MultiMap to) {
+        to.addAll(from);
+        hopByHopHeaders.forEach(to::remove);
+
+        String connectionHeader = from.get("connection");
+        if (connectionHeader != null) {
+            for (String name : connectionHeaderValueRE.split(connectionHeader.trim())) {
+                to.remove(name);
+            }
+        }
+    }
+
+    static final AtomicLong requestId = new AtomicLong(Clock.systemUTC().millis());
+
+    public void handle(final HttpServerRequest sreq, final boolean isTls) {
+        if (!sreq.headers().contains(requestIdHeader)) {
+            String reqId = Long.toString(requestId.getAndIncrement());
+            sreq.headers().add(requestIdHeader, reqId);
+        }
+        String chost = sreq.remoteAddress().host();
+
+        HttpServerResponse sres = sreq.response();
+        sres.headers().add("keep-alive", keepAliveHeaderValue);
+        sres.headers().add("connection", "keep-alive");
+        sreq.exceptionHandler(t -> errorHandler.fail(sreq, 500, RejectReason.incomingRequestFail, t.getMessage()));
+        Target nextHop = targetResolver.resolveNextHop(sreq, isTls);
+
+        if (nextHop == null) {
+            // in this case the resolveNextHop takes care of sending the response
+            return;
+        }
+
+        MultiMap sreqh = sreq.headers();
+        String origHost = sreqh.get("Host");
+        if (origHost == null) {
+            errorHandler.fail(sreq, 400, RejectReason.noHostHeader, null);
+            return;
+        }
+        HttpClientRequest creq = client.request(sreq.method(), nextHop.socketPort, nextHop.socketHost, nextHop.uri);
+        creq.handler(cres -> {
+            cres.exceptionHandler(t -> errorHandler.fail(sreq, 502, RejectReason.incomingResponseFail, t.getMessage()));
+
+            sres.setStatusCode(cres.statusCode());
+            sres.setStatusMessage(cres.statusMessage());
+            MultiMap headers = cres.headers();
+            copyEndToEndHeaders(headers, sres.headers());
+            sres.headers().add("keep-alive", keepAliveHeaderValue);
+            sres.headers().add("connection", "keep-alive");
+            if (!headers.contains("content-length")) {
+                sres.setChunked(true);
+            }
+
+            Pump resPump = Pump.pump(cres, sres);
+            cres.endHandler(v -> sres.end());
+            resPump.start();
+        });
+        creq.exceptionHandler(t -> {
+            errorHandler.fail(sreq, 502, RejectReason.outgoingRequestFail, t.getMessage());
+        });
+        MultiMap creqh = creq.headers();
+        copyEndToEndHeaders(sreqh, creqh);
+        creqh.set("Host", nextHop.hostHeader);
+        creqh.set("X-Host", origHost);
+        creqh.set("X-Forwarded-For", chost);
+        creqh.set("X-Forwarded-Proto", isTls ? "https" : "http");
+        String transferEncoding = sreqh.get("transfer-encoding");
+        if (transferEncoding != null && transferEncoding.contains("chunked")) {
+            creq.setChunked(true);
+        }
+        Pump reqPump = Pump.pump(sreq, creq);
+        sreq.endHandler(v -> creq.end());
+        reqPump.start();
+    }
+}
